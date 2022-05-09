ed77d7d721dbe8156e59d57e2bc1464142536b08
caio <caiosasia@gmail.com>
2022-05-09 16:04:55 -0300

    fixes for deploy

--- src/Context/Nebula.jsx
+++ src/Context/Nebula.jsx
@@ -10,7 +10,7 @@ const getNebula = async () => {
         // url: 'https://cluster.us.qlikcloud.com',
         // webIntegrationId: 'f-pW61I6iRHS0UUocC1bGLtuFD7_Wjym',
         // appId: '28ee7016-b117-4d3e-a791-447c88bf6c1d'
-        url: 'dev.clusterdesign.com.br',
+        url: 'https://dev.clusterdesign.com.br',
         appId: '0aaa136d-eb71-4d60-90b0-2d8c266b07bc'
     });
     return embed(app);
--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,31 +1,30 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Switch, Redirect, Route } from "react-router-dom";
 import { useNebula } from "../Context/Nebula";
 
-// pages 
+// pages
 import Home from "../Pages/Home";
 
 function Layout() {
   const { nebula } = useNebula();
 
   useEffect(() => {
-    console.log('nebula', nebula);
+    console.log("nebula", nebula);
   });
 
   return (
     <>
-      <>
-        <BrowserRouter>
-          <div className="layout">
-            <Routes>
-              <Route
-                path="/"
-                element={<Home />}
-              />
-            </Routes>
-          </div>
-        </BrowserRouter>
-      </>
+      <HashRouter>
+        <div className="layout">
+          <Switch>
+            <Route path="/home" render={() => <Home />} />
+            <Route path="/page2" render={() => <h1>page2</h1>} />
+            <Route exact path="/">
+              <Redirect to="/home" />
+            </Route>
+          </Switch>
+        </div>
+      </HashRouter>
     </>
   );
 }
