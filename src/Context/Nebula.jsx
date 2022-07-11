dda39d495522a624d450e903e27af777e87eeb4a
caio <caiosasia@gmail.com>
2022-07-11 13:08:09 -0300

    enigma app in context fix

--- src/Context/Nebula.jsx
+++ src/Context/Nebula.jsx
@@ -10,20 +10,24 @@ const getNebula = async () => {
         url: 'https://dev.clusterdesign.com.br',
         appId: '0aaa136d-eb71-4d60-90b0-2d8c266b07bc'
     });
-    return embed(app);
+    return { nebula: embed(app), app };
   };
 
 const NebulaContext = createContext();
 
 function NebulaProvider({ children }) {
     const [nebula, setNebula] = useState(null);
+    const [app, setApp] = useState(null);
 
     useEffect(() => {
-        getNebula().then(res => setNebula(res));
+      getNebula().then((res) => {
+        setNebula(res.nebula);
+        setApp(res.app);
+      });
     }, []);
 
     return (
-        <NebulaContext.Provider value={{ nebula }}>
+        <NebulaContext.Provider value={{ nebula, app }}>
             {children}
         </NebulaContext.Provider>
     );
